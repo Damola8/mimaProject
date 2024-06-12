f86024ea32ba82bd8b336d69072aac0fb0ce7e87
Damola8 <sulaimondamola96@gmail.com>
2024-06-12 22:33:46 +0100

    installed cypress

--- cypress/e2e/mimaproject.spec.cy.js
+++ cypress/e2e/mimaproject.spec.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('verify that user is able to signup successfully', () => {
+    cy.visit('/')
   })
 })
